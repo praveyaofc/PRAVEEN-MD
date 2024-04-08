eedfbfd94181d631facc1f1eadea2ff1789b6d58
PRAVEEN NIMSARA <151917057+praveyaofc@users.noreply.github.com>
2024-04-08 12:26:58 +0530

    Update auto_reply.js

--- whatsapp/auto_reply.js
+++ whatsapp/auto_reply.js
@@ -1,3 +1,1 @@
-{
-  "dark shan": "𝐻𝐸𝐿𝐿𝑂 𝐼𝑀 𝐷𝐴𝑅𝐾 𝑆𝐻𝐴𝑁 𝑌𝑇 \n\n𖣔ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ𖣔\nhttps://youtube.com/@darkshanyt1\n\n𖤍ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ɢʀᴏᴜᴘ𖤍\nhttps://chat.whatsapp.com/BxXB834YgV8JorX5tCDEEM\n\n☃︎ᴛʜᴀɴᴋs ғᴏʀ ʏᴏᴜ ᴍᴀssᴇɢᴇ ☃︎"
-}
+{}
